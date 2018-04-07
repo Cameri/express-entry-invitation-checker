b6a956c06e1e2eb265a193aef6a60345862a3a0a
Ricardo Arturo Cabral <ricardo.arturo.cabral@gmail.com>
2018-04-07 01:23:41 -0400

    Simplify extractData() in express-entry-rounds-service

--- src/express-entry-rounds-service.js
+++ src/express-entry-rounds-service.js
@@ -27,19 +27,12 @@ function parsePage (response) {
 
 function extractData (data) {
   logger.debug('extractData() data:', data)
-  const drawDateStr = data.draw_date.split(': ').pop()
-  const drawDate = moment.utc(drawDateStr, 'MMMM-DD-YYYY-HH:mm:ss')
-  const lowestCrs = parseInt(data.lowest_crs.split(': ').pop().replace(',', '').trim(), 10)
-  const invitationsIssued = parseInt(data.invitations_issued.match(/[0-9,]+/)[0].replace(',', ''), 10)
-  const tieBreakStr = data.tie_break.match(/:\s*(.+?)\s*UTC/)[1]
-  const tieBreak = moment.utc(tieBreakStr, 'MMMM-DD-YYYY-HH:mm:ss')
-  const dateModified = moment.utc(data.date_modified.match(/\d{4}-\d{2}-\d{2}/)[0])
   return {
-    draw_date: drawDate,
-    lowest_crs: lowestCrs,
-    invitations_issued: invitationsIssued,
-    tie_break: tieBreak,
-    date_modified: dateModified,
+    draw_date: moment.utc(data.draw_date.split(': ').pop(), 'MMMM-DD-YYYY-HH:mm:ss'),
+    lowest_crs: parseInt(data.lowest_crs.split(': ').pop().replace(',', '').trim(), 10),
+    invitations_issued: parseInt(data.invitations_issued.match(/[0-9,]+/)[0].replace(',', ''), 10),
+    tie_break: moment.utc(data.tie_break.match(/:\s*(.+?)\s*UTC/)[1], 'MMMM-DD-YYYY-HH:mm:ss'),
+    date_modified: moment.utc(data.date_modified.match(/\d{4}-\d{2}-\d{2}/)[0]),
     hash: data.hash
   }
 }
