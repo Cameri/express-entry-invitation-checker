fdd5c5acc957694fe68ad9d595414853c8352cbf
Ricardo Arturo Cabral <ricardo.arturo.cabral@gmail.com>
2018-10-30 01:18:04 -0400

    Convert moment to string when persisting draw to db

--- src/db/dao/draws-dao.js
+++ src/db/dao/draws-dao.js
@@ -1,7 +1,15 @@
 const moment = require('moment')
 
+const DATE_FMT = 'YYYY-MM-DD HH:mm:ss'
+
 function save (db, draw) {
-  return db('express_entry_draws').insert(draw)
+  const newDraw = Object.keys(draw)
+    .filter(key => draw[key] instanceof moment)
+    .reduce((prev, key) => {
+      prev[key] = prev[key].format(DATE_FMT)
+      return prev
+    }, {...draw})
+  return db('express_entry_draws').insert(newDraw)
 }
 
 function getLatestRecordedDraw (db) {
@@ -17,11 +25,14 @@ function getLatestRecordedDraw (db) {
     .orderBy('draws.date_modified', 'DESC')
     .limit(1)
     .then(([result]) => {
-      return Object.assign({}, result, {
-        draw_date: moment.utc(result.draw_date),
-        date_modified: moment.utc(result.date_modified),
-        tie_break: moment.utc(result.tie_break)
-      })
+      return {
+        ...result,
+        ...{
+          draw_date: moment.utc(result.draw_date),
+          date_modified: moment.utc(result.date_modified),
+          tie_break: moment.utc(result.tie_break)
+        }
+      }
     })
 }
 
